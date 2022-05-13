d492be9ffd6aa5a4d155e46a23c8656ebd9f4415
Peter Sekan <sekan@inqool.cz>
2022-05-13 12:56:18 +0200

    Build app without go:offline

--- api/feeder-api/src/main/java/cz/inqool/dl4dh/feeder/FeederApplication.java
+++ api/feeder-api/src/main/java/cz/inqool/dl4dh/feeder/FeederApplication.java
@@ -1,19 +1,19 @@
 package cz.inqool.dl4dh.feeder;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  * @author Peter Sekan
  */
-@SpringBootConfiguration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "cz.inqool.dl4dh.feeder")
+@EntityScan(basePackages = "cz.inqool.dl4dh.feeder")
+@SpringBootApplication
 public class FeederApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FeederApplication.class, args);
     }
 }
+
+
