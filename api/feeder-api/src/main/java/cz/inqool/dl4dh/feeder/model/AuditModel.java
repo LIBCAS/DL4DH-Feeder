a7696a00d7bfefb788461cce06d0fa2c66bebfaa
Peter Sekan <sekan@inqool.cz>
2023-03-09 11:24:13 +0100

    Audit model with createdAt as public field

--- api/feeder-api/src/main/java/cz/inqool/dl4dh/feeder/model/AuditModel.java
+++ api/feeder-api/src/main/java/cz/inqool/dl4dh/feeder/model/AuditModel.java
@@ -1,6 +1,8 @@
 package cz.inqool.dl4dh.feeder.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,36 +11,22 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+@Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(
-        value = {"createdAt", "updatedAt"},
+        value = {"updatedAt"},
         allowGetters = true
 )
 public abstract class AuditModel implements Serializable {
-//	@Temporal(TemporalType.TIMESTAMP)
-//    @Column(name = "created_at", nullable = false, updatable = false)
-//    @CreatedDate
-//    private Date createdAt;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    @Column(name = "updated_at", nullable = false)
-//    @LastModifiedDate
-//    private Date updatedAt;
-//
-//    public Date getCreatedAt() {
-//        return createdAt;
-//    }
-//
-//    public void setCreatedAt(Date createdAt) {
-//        this.createdAt = createdAt;
-//    }
-//
-//    public Date getUpdatedAt() {
-//        return updatedAt;
-//    }
-//
-//    public void setUpdatedAt(Date updatedAt) {
-//        this.updatedAt = updatedAt;
-//    }
+	@Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Date updatedAt;
 }
